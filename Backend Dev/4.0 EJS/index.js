abac714985da6bb4f8dcff219afa529456f3cce1
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-07-21 14:36:04 +0100

    ejs 4.0 final

--- Backend Dev/4.0 EJS/index.js
+++ Backend Dev/4.0 EJS/index.js
@@ -1,14 +1,7 @@
 import express from "express";
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import bodyParser from "body-parser";
-
-// const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-
-// app.use(bodyParser.urlencoded({extended: true}));
 
 var article = "a";
 var timeOfWeek = "day";
