70baae67634344f0e240766ae23b3fb9696e7337
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-07-23 23:17:06 +0100

    ToDo List Completed

--- Backend Dev/Capstone 3/index.js
+++ Backend Dev/Capstone 3/index.js
@@ -8,19 +8,30 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+const homeTasks = [];
+const workTasks = [];
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs",{tasks: homeTasks});
   });
 
 app.get("/work", (req, res) => {
-res.render("work.ejs");
+res.render("work.ejs",{tasks: workTasks});
 });
 
 app.get("/add", (req, res) => {
     res.render("add.ejs");
   });
+
+app.post("/",(req, res) => {
+  homeTasks.push(req.body["task"]);
+  res.render("index.ejs",{tasks: homeTasks});  }
+)
+
+app.post("/work",(req, res) => {
+  workTasks.push(req.body["task"]);
+  res.render("work.ejs",{tasks: workTasks});  }
+)
 
 
 app.listen(port, () => {
