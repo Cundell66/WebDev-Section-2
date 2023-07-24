be8aeaf0590aecc89ab06e317bc2a7684a2f976e
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-07-24 10:35:22 +0100

    Code tidied up & redundant code removed

--- Backend Dev/Capstone 3/index.js
+++ Backend Dev/Capstone 3/index.js
@@ -12,27 +12,32 @@ const homeTasks = [];
 const workTasks = [];
 
 app.get("/", (req, res) => {
-    res.render("index.ejs",{tasks: homeTasks});
+    // const allTasks = homeTasks.concat(workTasks);
+    res.render("index.ejs",{tasks: homeTasks.concat(workTasks)});
+  });
+
+app.get("/home", (req, res) => {
+    res.render("home.ejs",{tasks: homeTasks});
   });
 
 app.get("/work", (req, res) => {
-res.render("work.ejs",{tasks: workTasks});
-});
-
-app.get("/add", (req, res) => {
-    res.render("add.ejs");
+    res.render("work.ejs",{tasks: workTasks});
   });
 
 app.post("/",(req, res) => {
   homeTasks.push(req.body["task"]);
-  res.render("index.ejs",{tasks: homeTasks});  }
+  res.render("index.ejs",{tasks: homeTasks.concat(workTasks)});  }
+)
+
+app.post("/home",(req, res) => {
+  homeTasks.push(req.body["task"]);
+  res.render("home.ejs",{tasks: homeTasks});  }
 )
 
 app.post("/work",(req, res) => {
   workTasks.push(req.body["task"]);
   res.render("work.ejs",{tasks: workTasks});  }
 )
-
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
