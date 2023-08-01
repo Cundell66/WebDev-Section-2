8666d0ff79a4092eb4a9b88d056a9c24fa3b8e9b
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-08-01 16:54:21 +0100

    5.6 Secrets Project

--- Backend Dev/boilerplate.js
+++ Backend Dev/boilerplate.js
@@ -5,12 +5,14 @@ import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
