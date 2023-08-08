c02671d3f9df98c7e748784caef09328cd188ba1
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-08-08 11:36:11 +0100

    step one

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -1,68 +1,88 @@
 //jshint esversion:6
 
 import express from "express";
-import { urlencoded } from "body-parser";
-import { connect, model } from "mongoose";
+import bodyparser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(urlencoded({extended: true}));
+app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
-const itemsSchema = new Schema({
+const itemsSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Name Is Required"]
   }
   });
 
-const Item = model("Item", itemsSchema);
+const Item = mongoose.model("Item", itemsSchema);
 
-const shopping = new Item ({
-  name: "Go To Supermarket"
+const item1 = new Item ({
+  name: "Welcome to your ToDo List"
 });
 
-const cleaning = new Item ({
-  name: "Clean Bathroom"
+const item2 = new Item ({
+  name: "Hit the + button to add an item"
 });
 
-const cooking = new Item ({
-  name: "Prepare casserole"
+const item3 = new Item ({
+  name: "<-- hit this to delete an item"
 });
 
-const defaultItems = [shopping, cleaning, cooking];
+const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems)
-    .then(function(){
-        console.log("Successfully saved all the items to todolistDB");
-    })
-    .catch(function(err){
-        console.log(err);
-    });
+
 
 app.get("/", function(req, res) {
+  Item.find()
+    .then (items => {
+      if (items.length === 0){
+            Item.insertMany(defaultItems)
+        .then(function(){
+            console.log("Successfully saved all the items to todolistDB");
+        })
+        .catch(function(err){
+            console.log(err);
+        });
 
-  res.render("list", {listTitle: "Today", newListItems: items});
-
+      } else {
+        res.render("list", {listTitle: "Today", newListItems: items});
+      }
+    })  
 });
 
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item({
+    name: itemName
+  });
+
+
+  item.save();
+
+  res.redirect("/");
+
+  // if (req.body.list === "Work") {
+  //   workItems.push(item);
+  //   res.redirect("/work");
+  // } else {
+  //   items.push(item);
+  //   res.redirect("/");
+  // }
 });
+
+app.post("/delete", function(req, res){
+  const itemName = req.body;
+  console.log(itemName);
+})
 
 app.get("/work", function(req,res){
   res.render("list", {listTitle: "Work List", newListItems: workItems});
