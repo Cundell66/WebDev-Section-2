5eae2cec988f16a28e7e675f0b0fbe3c307223c1
Cundell66 <134798569+Cundell66@users.noreply.github.com>
2023-08-11 15:58:49 +0100

    secrets Leve 1 2 3

---
+++ Backend Dev/Secrets - Starting Code/app L2.js
@@ -0,0 +1,79 @@
+//jshint esversion:6
+
+import ejs from "ejs";
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import encrypt from "mongoose-encryption";
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}) );
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret ="Thisisourlittlesecret";
+userSchema.plugin(encrypt, {secret:secret, encryptedFields:["password"]});
+ 
+const User = mongoose.model("User", userSchema);
+
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+    newUser.save()
+    .then(()=>{
+        console.log("New User Added");
+        res.render("secrets");
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({email: username})
+    .then((foundUser)=>{
+        if (foundUser){
+            if (foundUser.password === password){
+                res.render("secrets");
+            } else {
+                res.redirect("login");
+            }
+        } else {
+            res.send("No Such User");
+        }
+      })
+      .catch((err)=>{
+        console.log(err);
+      });
+})
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000.");
+});
